0a802b2a4517ba3c427827ce19b7ff8a0ca9a1f7
oks234 <bestspx@gmail.com>
2020-01-03 13:53:33 +0900

    styled movie-detail link

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,25 +6,24 @@ import "./Movie.css";
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
     <Link
+      className="movie"
       to={{
         pathname: `/movie/${id}`,
         state: { year, title, summary, poster, genres }
       }}
     >
-      <div className="movie">
-        <img src={poster} alt={title} title={title} />
-        <div className="movie__data">
-          <h3 className="movie__title">{title}</h3>
-          <h5 className="movie__year">{year}</h5>
-          <ul className="movie__genres">
-            {genres.map((genre, index) => (
-              <li key={index} className="movie__genre">
-                {genre}
-              </li>
-            ))}
-          </ul>
-          <p className="movie__summary">{summary.slice(0, 180)} ...</p>
-        </div>
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="movie__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 180)} ...</p>
       </div>
     </Link>
   );
