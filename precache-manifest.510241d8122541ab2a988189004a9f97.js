c7a5bd14e876f5bfc38d9dcfc7c3ff07134bb6e3
oks234 <bestspx@gmail.com>
2020-01-03 09:53:28 +0900

    Updates

---
+++ precache-manifest.510241d8122541ab2a988189004a9f97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb67de9d7feb679da92c5a52b54ced5a",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "270bc3abec52ed9aeba9",
+    "url": "/movie_app_2019/static/css/main.d10d8a8b.chunk.css"
+  },
+  {
+    "revision": "8526568d949b0ffebe66",
+    "url": "/movie_app_2019/static/js/2.a187f9e2.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app_2019/static/js/2.a187f9e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "270bc3abec52ed9aeba9",
+    "url": "/movie_app_2019/static/js/main.435fecbe.chunk.js"
+  },
+  {
+    "revision": "101d7acd549509bcf93b",
+    "url": "/movie_app_2019/static/js/runtime-main.69d003e7.js"
+  }
+]);
